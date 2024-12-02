062a40c414e2508c6dfa70c54d68e5690b56598f
Ezzagmoute Hamza <ezzagmoute.hamza@gmail.com>
2024-12-02 16:12:36 +0100

    added services data integration

--- src/App.vue
+++ src/App.vue
@@ -13,134 +13,147 @@
       <div class="relative bg-white p-6 rounded-lg shadow">
         <h2 class="text-xl font-semibold mb-4 text-gray-700">Ventes totales</h2>
         <p class="text-3xl font-bold text-blue-600">{{ totalSales.toFixed(2) }}{{ currency }}</p>
+        <Loader v-if="apiServicesLoadingStatus.totalSales" />
       </div>
       
       <div class="relative bg-white p-6 rounded-lg shadow">
         <h2 class="text-xl font-semibold mb-4 text-gray-700">Produits les plus vendus</h2>
         <ul class="space-y-2">
-          <li v-for="product in topProducts" :key="product.id" class="flex justify-between">
-            <span class="text-gray-600">{{ product.name }}</span>
-            <span class="font-medium text-gray-800">{{ product.quantitySold }} unités</span>
+          <li v-for="product in trendingProducts" :key="product.ProductName" class="flex justify-between">
+            <span class="text-blue-600 font-bold">{{ product.ProductName }}</span>
+            <span class="font-medium text-gray-800">{{ product.totalQuantity }} {{unit}}</span>
           </li>
         </ul>
+        <Loader v-if="apiServicesLoadingStatus.trendingProducts"/>
       </div>
       
       <div class="relative bg-white p-6 rounded-lg shadow">
         <h2 class="text-xl font-semibold mb-4 text-gray-700">Répartition des ventes par catégorie</h2>
-        <ul class="space-y-2">
-          <li v-for="(sales, category) in categorySales" :key="category" class="flex justify-between">
-            <span class="text-gray-600">{{ category }}</span>
-            <span class="font-medium text-gray-800">{{ 10 }} %</span>
+        <div class="space-y-2">
+          <li v-for="category in categorySales" :key="category.category" class="grid grid-cols-12">
+            <span class="text-blue-600 font-bold col-span-6">{{ category.category }}</span>
+            <span class="font-medium text-gray-600 col-span-4">{{ category.salesCount }}</span>
+            <span class="col-span-2 font-bold text-blue-600">{{ category.percentage.toFixed(2) }} %</span>
           </li>
-        </ul>
+        </div>
+        <Loader v-if="apiServicesLoadingStatus.categorySales" />
       </div>
+
     </div>
     
     <div class="relative bg-white p-6 rounded-lg shadow overflow-x-auto">
       <h2 class="text-xl font-semibold mb-4 text-gray-700">Liste des produits</h2>
       <table class="w-full">
         <thead>
-          <tr class="text-left bg-gray-50">
+          <tr class="text-left font-bold  bg-gray-50 text-blue-700">
             <th class="p-2">Nom du produit</th>
-            <th class="p-2">Date d'ajout</th>
-            <th class="p-2">Prix</th>
-            <th class="p-2">Ventes totales</th>
+            <th class="p-2">Prix<span>({{currency}})</span></th>
+            <th class="p-2">Ventes totales ({{unit}})</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="product in products" :key="product.id" class="border-t">
-            <td class="p-2">{{ product.name }}</td>
-            <td class="p-2">{{ product.dateAdded }}</td>
+            <td class="p-2 text-blue-600 font-medium">{{ product.productName }}</td>
             <td class="p-2">{{ product.price }}</td>
-            <td class="p-2">{{ product.quantitySold }}</td>
+            <td class="p-2">{{ product.totalSales }}</td>
           </tr>
         </tbody>
       </table>
+      <Loader v-if="apiServicesLoadingStatus.products" />
     </div>
-
   </div>
 </template>
 
 <script setup>
-  
-    import { ref, computed } from 'vue'
+import { ref, reactive, computed, onMounted } from 'vue';
+import axios from 'axios';
+import {useToast} from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
 
-    const products = ref([
-      {
-        id: '1',
-        name: 'Product A',
-        price: 99.99,
-        category: 'Electronics',
-        dateAdded: '2024-01-15',
-        totalSales: 4999.50,
-        quantitySold: 50
-      },
-      {
-        id: '2',
-        name: 'Product B',
-        price: 49.99,
-        category: 'Clothing',
-        dateAdded: '2024-01-20',
-        totalSales: 2499.50,
-        quantitySold: 50
-      },
-      {
-        id: '3',
-        name: 'Product C',
-        price: 29.99,
-        category: 'Books',
-        dateAdded: '2024-01-25',
-        totalSales: 899.70,
-        quantitySold: 30
-      },
-      {
-        id: '4',
-        name: 'Product D',
-        price: 199.99,
-        category: 'Electronics',
-        dateAdded: '2024-02-01',
-        totalSales: 5999.70,
-        quantitySold: 30
-      },
-      {
-        id: '5',
-        name: 'Product E',
-        price: 15.99,
-        category: 'Books',
-        dateAdded: '2024-02-05',
-        totalSales: 479.70,
-        quantitySold: 30
-      }
-    ]);
 
-    const currency = ' MAD';
+import Loader from './components/Loader.vue';
 
-    const timeRange = ref('7d');
+const backendUrl = import.meta.env.VITE_BACKEND_SERVER_URL;
+const products = ref([]);
+const totalSales = ref(0);
+const trendingProducts = ref([]);
+const categorySales = ref([]);
+const currency = ' DH';
+const unit = ' unités';
+const timeRange = ref('7d');
 
-    const handleTimeRange = computed(()=>{
-      console.log(timeRange._value);
-    });
-    
-    const topProducts = computed(() => {
-      return [...products.value]
-        .sort((a, b) => b.quantitySold - a.quantitySold)
-        .slice(0, 5)
-    });
+const $toast = useToast();
 
-    const categorySales = computed(() => {
-      return products.value.reduce((acc, product) => {
-        const sales = product.price * product.quantitySold
-        acc[product.category] = (acc[product.category] || 0) + sales
-        return acc
-      }, {})
-    });
-    const calculatePercentage = (categoryTotal) => {
-      const total = Object.values(categorySales.value).reduce((sum, sales) => sum + sales, 0)
-      return ((categoryTotal / total) * 100).toFixed(1)
-    }
+const displayToastError = (message)=>{
+  $toast.open({
+    message:`Failed to fetch ${message} Please try again later.`,
+    type:"error",
+    position:"top"
+  });
+}
+const apiServicesLoadingStatus = reactive({
+  products: false,
+  totalSales: false,
+  trendingProducts: false,
+  categorySales: false,
+});
+const handleTimeRange = ()=>{
+  fetchTotalSales(timeRange.value);
+};
+const fetchProducts = async () => {
+  apiServicesLoadingStatus.products = true;
+  try {
+    const response = await axios.get(`${backendUrl}/products`);
+    products.value = response.data.products;
+  } catch (err) {
+    displayToastError("products ");
+    console.error(err);
+  } finally {
+    apiServicesLoadingStatus.products = false;
+  }
+};
+const fetchTotalSales = async (period = timeRange.value) => {
+  apiServicesLoadingStatus.totalSales = true;
+  try {
+    const response = await axios.get(`${backendUrl}/analytics/total_sales?period=${period}`);
+    totalSales.value = response.data.totalSales;
+  } catch (err) {
+    displayToastError("Total sales");
+    console.error(err);
+  } finally {
+    apiServicesLoadingStatus.totalSales = false;
+  }
+};
+const fetchTrendingProducts = async () => {
+  apiServicesLoadingStatus.trendingProducts = true;
+  try {
+    const response = await axios.get(`${backendUrl}/analytics/trending_products`);
+    trendingProducts.value = response.data.trendingProducts;
+  } catch (err) {
+    displayToastError("Trending products");
+    console.error(err);
+  } finally {
+    apiServicesLoadingStatus.trendingProducts = false;
+  }
+};
+const fetchCategorySales = async () => {
+  apiServicesLoadingStatus.categorySales = true;
+  try {
+    const response = await axios.get(`${backendUrl}/analytics/category_sales`);
+    categorySales.value = response.data.categorySales;
+  } catch (err) {
+    displayToastError("Category sales");
+    console.error(err);
+  } finally {
+    apiServicesLoadingStatus.categorySales = false;
+  }
+};
 
-    const totalSales = computed(() => {
-      return products.value.reduce((sum, product) => sum + (product.price * product.quantitySold), 0)
-    });
+onMounted(() => {
+  fetchCategorySales();
+  fetchTotalSales();
+  fetchTrendingProducts();
+  fetchProducts();
+});
 
 </script>
