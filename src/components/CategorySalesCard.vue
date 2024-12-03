bfdacf00bed647139a7e8c01226b0352f5aead0e
Ezzagmoute Hamza <ezzagmoute.hamza@gmail.com>
2024-12-03 16:56:49 +0100

    added the pie chart to the categorySales

--- src/components/CategorySalesCard.vue
+++ src/components/CategorySalesCard.vue
@@ -2,23 +2,7 @@
   <div class="relative bg-white p-6 rounded-lg shadow">
     <h2 class="text-xl font-semibold mb-4 text-gray-700">{{ title }}</h2>
     <div class="space-y-2">
-      <ul>
-        <li
-          v-for="category in categorySales"
-          :key="category.category"
-          class="grid grid-cols-12"
-        >
-          <span class="text-blue-600 font-bold col-span-6">
-            {{ category.category }}
-          </span>
-          <span class="font-medium text-gray-600 col-span-4">
-            {{ category.salesCount }}
-          </span>
-          <span class="col-span-2 font-bold text-blue-600">
-            {{ category.percentage.toFixed(2) }} %
-          </span>
-        </li>
-      </ul>
+      <Pie :data="computedData" :options="options" />
     </div>
     <Loader v-if="isLoading" />
   </div>
@@ -26,8 +10,13 @@
 
 <script setup>
 import Loader from "./Loader.vue";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { computed } from "vue";
+import { Pie } from 'vue-chartjs';
 
-defineProps({
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+const props = defineProps({
   title: {
     type: String,
     default: "Répartition des ventes par catégorie",
@@ -42,4 +31,22 @@ defineProps({
     default: false,
   },
 });
+
+const computedData = computed(() => {
+  return {
+    labels: props.categorySales.map(category => category.category),
+    datasets: [
+      {
+        backgroundColor: ["#FF5733","#33FF57","#3357FF","#F1C40F","#8E44AD","#1ABC9C","#E74C3C","#3498DB","#2ECC71","#F39C12"],
+        data: props.categorySales.map(category => category.percentage.toFixed(2))
+      }
+    ]
+  }
+})
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false
+}
+
 </script>
